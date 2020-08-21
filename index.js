9c6029f7818cbf48840cb8e4d67b0c62e6379ec4
Dean Summer <deanlovesmargie@gmail.com>
2020-08-21 18:45:34 -0500

    Responds to messages

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
+client.on('message', message => {
+    if (message.author.bot) return;
+    message.channel.send('You sent a message. I also checked to see if you\'re a bot or I would loop forever.');
+});
+
 client.login(token);
