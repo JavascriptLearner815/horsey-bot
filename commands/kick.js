128a158f21d3375e623c9a2f7c4c19225c248754
Dean Summer <deanlovesmargie@gmail.com>
2020-08-21 20:35:22 -0500

    kick command

---
+++ commands/kick.js
@@ -0,0 +1,40 @@
+module.exports = {
+    name: 'kick',
+    description: 'Kick a member of the server.',
+    args: true,
+    usage: '<user> <reason>',
+    guildOnly: true,
+    cooldown: 3,
+    aliases: ['remove'],
+    execute(message, args) {
+        const member = message.mentions.members.first() || args[0];
+        let reason = args.slice(1).join(' ');
+
+        if (!message.guild.member(message.author).hasPermission('KICK_MEMBERS')) {
+            return message.channel.send('You don\'t have the permissions to kick, I\'m not a dumb horsey!');
+        }
+
+        if (!message.guild.me.hasPermission('KICK_MEMBERS')) {
+            return message.channel.send('I can\'t kick members.');
+        }
+
+        if (member.user.id === message.guild.ownerID) {
+            return message.channel.send('You can\'t kick the owner.');
+        }
+
+        if (!reason) {
+            reason = 'Unspecified';
+        }
+
+        member.kick(reason);
+
+        try {
+            member.user.send(`You were kicked from ${message.guild.name} by ${message.author} for ${reason}.`);
+        } catch (err) {
+            console.error('Couldn\'t DM kick message', err);
+            message.channel.send(`${member.user} kicked for ${reason}.`);
+        } finally {
+            message.channel.send('Kick successful!');
+        }
+    },
+};
--- index.js
+++ index.js
@@ -84,4 +84,12 @@ client.on('message', message => {
     }
 });
 
+client.on('shardError', error => {
+    console.error('A websocket connection encountered an error:', error);
+});
+
+process.on('unhandledRejection', error => {
+    console.error('Unhandled promise rejection:', error);
+});
+
 client.login(token);
