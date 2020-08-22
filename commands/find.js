0f4102c50d7e9c503cd00b9300ee05fbd2c3c038
Dean Summer <deanlovesmargie@gmail.com>
2020-08-22 14:04:09 -0500

    test

---
+++ commands/find.js
@@ -0,0 +1,35 @@
+module.exports = {
+    name: 'find',
+    description: 'Look for a horsey.',
+    args: false,
+    usage: false,
+    guildOnly: false,
+    cooldown: 30,
+    aliases: ['recruit'],
+    execute(message, args) {
+        const horseyOne = 'cactus';
+        const horseyTwo = 'horse';
+        const horseyThree = 'pony';
+        const horseyFour = 'zaneanderman';
+        const horseyFive = 'god';
+        let horsey = null;
+
+        const min = Math.ceil(1);
+        const max = Math.floor(15);
+        const random = Math.floor(Math.random() * (max - min + 1) + min); 
+
+        if (random === 1 || random === 2 || random === 3 || random === 4 || random === 5) {
+            horsey = horseyOne;
+        } else if (random === 6 || random === 7 || random === 8 || random === 9) {
+            horsey = horseyTwo;
+        } else if (random === 10 || random === 11 || random === 12) {
+            horsey = horseyThree;
+        } else if (random === 13 || random === 14) {
+            horsey = horseyFour;
+        } else if (random === 15) {
+            horsey = horseyFive;
+        }
+
+        return message.reply(`You found a ${horsey} horsey!`);
+    },
+};
