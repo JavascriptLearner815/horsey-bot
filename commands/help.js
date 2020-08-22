21a23e2ac48f646b4d534f792e9da3791c9b3854
Dean Summer <deanlovesmargie@gmail.com>
2020-08-21 20:12:32 -0500

    addition

--- commands/help.js
+++ commands/help.js
@@ -27,11 +27,28 @@ module.exports = {
                 .setFooter(footer);
 
             try {
-                return message.channel.send(helpEmbed);
+                return message.channel.send(helpEmbed, { split: true });
             } catch (err) {
                 console.error('Could not send help embed!\n', err);
                 message.channel.send('Oh no! I couldn\'t send my embed!');
             }
         }
+
+        const name = args[0].toLowerCase();
+        const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
+        
+        if (!command) {
+            return message.reply('that\'s not a valid command!');
+        }
+        
+        data.push(`**Name:** ${command.name}`);
+        
+        if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
+        if (command.description) data.push(`**Description:** ${command.description}`);
+        if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
+        
+        data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
+        
+        message.channel.send(data, { split: true });
     },
 };
