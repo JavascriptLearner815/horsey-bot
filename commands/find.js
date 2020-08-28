5fcde54602149ef6490423cab8228fcd35cc9628
Dean Summer <deanlovesmargie@gmail.com>
2020-08-28 18:32:09 -0500

    add 4 more horsey

--- commands/find.js
+++ commands/find.js
@@ -13,22 +13,34 @@ module.exports = {
         const horseyThree = 'pony';
         const horseyFour = 'zaneanderman';
         const horseyFive = 'god';
+        const horseySix = 'rrs';
+        const horseySeven = 'bot';
+        const horseyEight = 'sweaty';
+        const horseyNine = 'no-name';
         let horsey = null;
 
         const min = Math.ceil(1);
-        const max = Math.floor(15);
+        const max = Math.floor(101);
         const random = Math.floor(Math.random() * (max - min + 1) + min); 
 
-        if (random === 1 || random === 2 || random === 3 || random === 4 || random === 5) {
+        if (random >= 1 && random < 30) {
             horsey = horseyOne;
-        } else if (random === 6 || random === 7 || random === 8 || random === 9) {
+        } else if (random >= 30 && random < 50) {
             horsey = horseyTwo;
-        } else if (random === 10 || random === 11 || random === 12) {
+        } else if (random >= 50 && random < 65) {
             horsey = horseyThree;
-        } else if (random === 13 || random === 14) {
+        } else if (random >= 65 && random < 80) {
             horsey = horseyFour;
-        } else if (random === 15) {
+        } else if (random >= 80 && random < 87) {
             horsey = horseyFive;
+        } else if (random >= 87 && random < 92) {
+            horsey = horseySix;
+        } else if (random >= 92 && random < 96) {
+            horsey = horseySeven;
+        } else if (random >= 96 && random < 99) {
+            horsey = horseyEight;
+        } else if (random >= 99 && random < 101) {
+            horsey = horseyNine;
         }
 
         return message.reply(`you found a ${horsey} horsey!`);
