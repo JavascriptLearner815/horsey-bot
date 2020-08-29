f3b239f0cd6780a6d077996bc7dd0e2997c487cc
Dean Summer <deanlovesmargie@gmail.com>
2020-08-29 12:38:05 -0500

    random dog

---
+++ commands/dog.js
@@ -0,0 +1,17 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+	name: 'dog',
+	description: 'Get a random dog from the REST API. Fetch! Haha.',
+	args: false,
+	usage: false,
+	guildOnly: false,
+	cooldown: 10,
+	cooldownMessage: 'Aww, slow down dog lover',
+	aliases: ['fetch', 'puppy', 'puppie'],
+	async execute(message, args) {
+		const dog = await fetch('https://dog.ceo/api/breeds/image/random').then(response => response.json());
+
+		message.channel.send(dog.message);
+	},
+};
